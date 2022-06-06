b7584956648be3294cbf33bfe0aa7e2026962699
Mahmoud Hashim <Mahmoud.ios.dev@gmail.com>
2022-06-06 12:04:29 +0200

    user can search for other users

--- twitterClone/Search/View/SearchVC+TableView.swift
+++ twitterClone/Search/View/SearchVC+TableView.swift
@@ -10,26 +10,25 @@ import UIKit
 
 extension SearchViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return arrUsers.count
+        return filterdUser.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        let users = arrUsers[indexPath.row]
-        if arrUsers.count > 0 {
-            cell.textLabel?.text = users.username
+        
+        if filterdUser.count > 0 {
+            cell.textLabel?.text = filterdUser[indexPath.row].username
             cell.imageView?.image = UIImage(named: "twitterEgg")
         }else {
             cell.textLabel?.text = "write anything in the search bar"
-
         }
-        print(users.email ?? "" )
+        print(filterdUser[indexPath.row].email ?? "" )
         return cell
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
-        print(arrUsers[indexPath.row])
+        print(filterdUser[indexPath.row].email)
     }
     
     
--- twitterClone/Search/View/SearchVC+TextField.swift
+++ twitterClone/Search/View/SearchVC+TextField.swift
@@ -10,30 +10,33 @@ import UIKit
 extension SearchViewController: UITextFieldDelegate{
 
     func textFieldDidChangeSelection(_ textField: UITextField) {
-        if textField.text != "" {
-            print("User is writing..")
-            
-        }else {
-            print("Empty the array")
+        if textField.text == "" {
+            filterdUser.removeAll()
+            tableView.reloadData()
         }
     }
     
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
         let searchtext = searchTxtField.text! + string  // the inserted text
-        if isWritten {
+        if textField.text != "" {       // it keeps refreshing whenever user writting
             
-        }else {
-            
+            filterdUser.removeAll()
+            let filtered = arrUsers.filter { (userTweet) -> Bool in
+                (userTweet.username?.contains(searchtext) ?? false)
+            }
+            for user in filtered {
+                filterdUser.append(user)
+                print(user.username!)
+            }
+            tableView.reloadData()
+            print("Search for a user")
+        }else{
+            filterdUser.removeAll()
+            tableView.reloadData()
+            print("Empty the array")
+
         }
-        let filtered = arrUsers.filter { (userTweet) -> Bool in
-            (userTweet.username?.contains(searchtext) ?? false)
-        }
-        for user in filtered {
-            filterdUser.append(user)
-            print(user.username)
-        }
-        
         return true
     }
 }
